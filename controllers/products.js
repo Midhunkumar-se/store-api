86038f6903f9fbf41f16f3cb83b65c33569119b5
Midhunkumar-se <midhu025@gmail.com>
2024-04-01 13:10:36 +0530

    Connected to MongoDB database , created error middleware, controller, route and basic configuration

---
+++ controllers/products.js
@@ -0,0 +1,16 @@
+const Product = require("../models/product");
+
+const getAllProductStatic = async (req, res) => {
+  const products = await Product.find({
+    featured: true,
+  });
+  res.status(200).json({ products, nbHits: products.length });
+};
+const getAllProducts = async (req, res) => {
+  res.status(200).json({ msg: "products route" });
+};
+
+module.exports = {
+  getAllProducts,
+  getAllProductStatic,
+};
